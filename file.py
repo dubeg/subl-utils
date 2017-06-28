dd732a2f305f84d8999845d3cc63d2fed59da413
Guillaume Dubé <gdube@plb.local>
2017-06-28 14:59:44 -0400

    Add popout file command

--- file.py
+++ file.py
@@ -1,6 +1,7 @@
 import sublime
 import sublime_plugin
 import os
+import subprocess
 
 # ---------------------------------------------------
 # Make RenameFile available as window command.
@@ -9,3 +10,17 @@ class RenameFileCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		filename = self.window.active_view().file_name()
 		self.window.run_command("rename_path", {"paths":[filename]})
+
+# ---------------------------------------------------
+# Pop out current file in new window.
+# ---------------------------------------------------
+class PopOutFileCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		window = self.window
+		view = window.active_view()
+		filename = view.file_name()
+		if filename != None and filename != "":
+			subprocess.Popen(["subl", "-n", filename], shell=True)
+			window.status_message("Pop: popped.")
+		else:
+			window.status_message("Pop: unsaved file not supported.")
