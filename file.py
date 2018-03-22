d690b72428317df45b2fed9eb6f1fde962e36c80
gdube <gdube@plbint.com>
2018-03-22 11:21:32 -0400

    Change command name to differentiate with default pkg's FileRename cmd

--- file.py
+++ file.py
@@ -6,7 +6,7 @@ import subprocess
 # ---------------------------------------------------
 # Make RenameFile available as window command.
 # ---------------------------------------------------
-class RenameFileCommand(sublime_plugin.WindowCommand):
+class RenameFileUtilCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		filename = self.window.active_view().file_name()
 		self.window.run_command("rename_path", {"paths":[filename]})
