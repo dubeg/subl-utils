2522f9b6a92598f1287e2f662fe1774518b20b62
Guillaume Dube <gdube@sajb.ca>
2021-10-07 13:52:14 -0400

    Fix custom sidebar commands and add "open file" using its default program

--- open.py
+++ open.py
@@ -92,3 +92,19 @@ class OpenFolder(sublime_plugin.WindowCommand):
                 "open_dir",
                 {"dir": os.path.dirname(path), "file": os.path.basename(path)}
             )
+
+
+# --------------------------------
+# Open file using default program.
+# --------------------------------
+class OpenFileUsingShell(sublime_plugin.WindowCommand):
+    def run(self, files):
+        for x in files:
+            os.startfile(x)
+    
+    def is_visible(self, files):
+        if len(files) > 0:
+            x = files[0]
+            if os.path.isdir(x):
+                return False
+        return True
